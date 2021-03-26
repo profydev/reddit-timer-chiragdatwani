75868116386effdbb002e5142854a63182263745
Chirag Datwani <chiragdatwani01@gmail.com>
2021-03-26 22:07:43 +0100

    added react-router-dom and routes to Home and Search pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './GlobalStyle';
 import theme from './theme';
@@ -7,9 +8,14 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div>
-        App Placeholder
-      </div>
+      <Switch>
+        <Route path="/" exact>
+          Home
+        </Route>
+        <Route path="/search">
+          Search
+        </Route>
+      </Switch>
     </ThemeProvider>
   );
 }
