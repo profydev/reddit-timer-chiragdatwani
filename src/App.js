de6287b12dc7a5292039dc5c93e8295891b34231
Chirag Datwani <chiragdatwani01@gmail.com>
2021-03-26 22:40:37 +0100

    added the Header component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './GlobalStyle';
+import Header from './Header';
 import theme from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Header />
       <BrowserRouter>
         <Switch>
           <Route path="/" exact>
